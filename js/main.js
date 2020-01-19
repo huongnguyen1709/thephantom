757f8820cd941e2b277210e2f1685ee0c1620125
huongnguyen1709 <38068008+huongnguyen1709@users.noreply.github.com>
2020-01-19 19:10:16 +0200

    animations & responsive

--- js/main.js
+++ js/main.js
@@ -1,50 +1,101 @@
-$(document).ready(function() {
-    $(window).scroll( function(){
+$(document).ready(function() {   
+
+    /* LOADING PROGRESS */ 
+
+    $('.home').hide();
+    $('.main').hide();
+
+    setTimeout(function(){
+        $('.loading-bar').css('opacity', '1');
+        setTimeout(function(){
+            $('.loading-fill').animate({'width':'30%',},800, function() {
+                setTimeout(function(){
+    
+                    $('.loading-fill').animate({'width':'40%',},1300);  
+    
+                    $('.loading-fill').animate({'width':'75%',},600, function() {
+                        setTimeout(function(){
+                            $('.loading-fill').animate({'width':'85%',},1100);
+                            $('.loading-fill').animate({'width':'100%',},200, function() {
+                                $('.loading').hide();
+                                $('.home').show();
+                                $('.main').show();
+                            });
+                        }, 300);
+                    });
+    
+                }, 500);
+                
+            });
+        }, 1500);
+        
+    }, 400);
+
+
+    /* ANIMATION FOR HIDING HEADER & SHOWING MAIN BODY */
+
+    setTimeout(function(){
+        $('.home').addClass('hiding-home');
+        $('html, body').animate({ scrollTop: 1000 }, 1000, function() {
+            $('.home').removeClass('hiding-home');
+            $('.main').addClass('showing-main');
+        });
        
+       
+    }, 10500);
+
+
+    /* ANIMATION FOR HEADER-TEXT & NAVIGATION */
+
+    setTimeout(function(){
+        $('.header-text').toggleClass('slide-header');
+        $('.logo').toggleClass('scale-show');
+        setTimeout(function(){
+            $('.nav-list').css('opacity', '1');
+            $('.languages').css('opacity', '1');
+        }, 300);
+        
+    }, 8000);
+
+
+    /* WINDOW SCROLL & ANIMATIONS FOR SECTIONS HAPTICS, FEATURES & STATEMENT */ 
+
+    $(window).scroll( function(){
+
         $('#bar-fill-1').each( function(i){
 
-            const top_of_element = $(this).height();;
-            const bottom_of_window = $(window).scrollTop() + $(window).height();
-            
+            const top_of_element = $(this).height();
+            const bottom_of_window = $(window).scrollTop(); 
+
             if( bottom_of_window > top_of_element ){
-                $(this).animate({'height':'100%',},3000, function() {
+
+                $(this).animate({'height':'100%',},5000, function() {
                     $(this).hide();
                     
-                    $('#haptics-bar').animate({'height':'5rem'},100);
-                    $('#haptics-text').animate({'opacity':'.5'},100, function() {
-                        
-                        $('#features-bar').css('height', '5rem');
-                        $('#section-features').animate({'top':'90%',},100, function() {
-                            $('#haptics-bar').animate({'height':'3rem'},100);
-                        });
+                    $('#haptics-bar').css('height', '3rem');
+                    $('.haptics-text').toggleClass('blur');
+                    $('#features-bar').css('height', '12rem');
+                    $('.section-features').css('top', '70%');
+                    $('.features-text').toggleClass('slide');
+                    $('#bar-fill-2').animate({'height':'100%',},5000, function() {
+                        $('#bar-fill-2').hide();
+                        $('#features-bar').css('height', '3rem');                 
+                        $('.features-text').toggleClass('slide');
+                        $('.features-text').toggleClass('blur');
+                        $('#statement-bar').css('height', '12rem');
+                        $('.section-statement').css('top', '100%');
+                        $('.statement-text').toggleClass('slide');
+                        $('#bar-fill-3').animate({'height':'100%',},5000);
                     });
-                    
-                    $('#haptics-text').animate({'opacity':'0'},100, function() {
-                        
-                        $('#features-bar').css('height', '11rem');
-                        $('#section-features').animate({'top':'70%',},100);
-                        $('#bar-fill-2').animate({'height':'100%',},3000, function() {});
-                        
-                        $('#features-text').animate({'opacity':'1',},500, function() {
-                           
-                        });
 
-                      
-                    });
-                    
-                   
-                    /*$('#section-features').animate({'top':'70%',},300);*/
-                    /*$('#features-text').animate({'opacity':'.5',},700, function() {
-                        $('#features-text').animate({'opacity':'1',},700);
-                    });*/
-                    
-                    
                 });
 
                 $(window).off('scroll');
             }
             
         });
-       
+        
     })
+
+    
 })
